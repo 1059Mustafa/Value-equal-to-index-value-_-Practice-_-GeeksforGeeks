c994866421bc616467ff61d318dd4c6a97771d7c
1059Mustafa <72117500+1059Mustafa@users.noreply.github.com>
2021-02-01 19:51:25 +0530

    Add files via upload

---
+++ sort_2.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:
+
+	vector<int> valueEqualToIndex(int arr[], int n) 
+	{
+	    int l=0;
+	    int r=n-1;
+	    vector<int>v;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]==i+1)
+	        {
+	            v.push_back(i+1);
+	        }
+	    }
+	    return v;
+	}
+};
